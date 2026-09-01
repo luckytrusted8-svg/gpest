3176f9da394922b61b8dd4bf23b0bf9805164bd0
Lucky Ubaidillah <luckytrusted8@gmail.com>
2026-09-01 14:58:50 +0700

    chore: remove dummy seeders and reset database to clean state

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolePermissionSeeder::class,
+            MasterDataSeeder::class,
             UserSeeder::class,
-            CustomerUserSeeder::class,
         ]);
     }
 }
